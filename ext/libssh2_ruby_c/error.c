9bfd224d86b86b2ad6d6f53e21b56b2d4accf668
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-02-26 19:54:45 -0800

    Comments

--- ext/libssh2_ruby_c/error.c
+++ ext/libssh2_ruby_c/error.c
@@ -1,5 +1,9 @@
 #include <libssh2_ruby.h>
 
+/*
+ * This is a convenience method to quickly wrap a libssh2 error in
+ * the proper class.
+ * */
 VALUE libssh2_ruby_wrap_error(int error) {
     VALUE instance = rb_class_new_instance(0, 0, rb_eLibSSH2_Native_Error);
     rb_iv_set(instance, "@error_code", INT2NUM(error));
--- lib/libssh2/native/error.rb
+++ lib/libssh2/native/error.rb
@@ -2,6 +2,7 @@ require "libssh2/native/error_codes"
 
 module LibSSH2
   module Native
+    # The exception raised if anything erroneous occurs.
     class Error < StandardError
       attr_reader :error_code
 
